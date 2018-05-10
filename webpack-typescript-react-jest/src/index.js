6a705f950ed0e74ab2673d0225c735e4f8d688f6
Satoshi Kawabata <satoshi.kawabata@nulab-inc.com>
2018-05-10 18:01:08 +0900

    initial build

---
+++ webpack-typescript-react-jest/src/index.js
@@ -0,0 +1,3 @@
+const h1 = document.createElement("h1");
+h1.innerText = "HelloWorld";
+document.body.appendChild(h1);
---
+++ webpack-typescript-react-jest/webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'index.js'
+  },
+  devServer: {
+    contentBase: "./dist",
+    port: "8888"
+  }
+};
