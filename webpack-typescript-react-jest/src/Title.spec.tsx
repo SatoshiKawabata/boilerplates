0af79b4d4c32c8992754dad8d57d3c90b2d71898
Satoshi Kawabata <satoshi.kawabata@nulab-inc.com>
2018-05-11 09:24:38 +0900

    add jest-css-modules

--- webpack-typescript-react-jest/src/Title.spec.tsx
+++ webpack-typescript-react-jest/src/Title.spec.tsx
@@ -14,6 +14,5 @@ describe("Title component test.", () => {
   it("should render correctly.", () => {
     const title = document.querySelector("h1");
     expect(title.textContent).toBe("HelloWorld");
-    expect(title.className).toBe("title");
   });
 });
